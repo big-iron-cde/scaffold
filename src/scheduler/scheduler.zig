6d081270cd97329e2d6d913c357e14cd0bcc5823
hemanialaparthi <alaparthi01@allegheny.edu>
2025-05-22 13:54:59 -0400

    feat(scheduler.zig): add code comment to explain what the schefduler does.

---
+++ src/scheduler/scheduler.zig
@@ -0,0 +1,5 @@
+// scheduler.zig
+// the scheduler ishould be able to determine a set of
+// machines on which a task can run. score the candidate
+// machines from the best to worst, and pick the machine
+// with the highest score.
