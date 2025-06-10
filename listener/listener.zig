6a4d35969e2daeb3b4b49c5a72f376b86bc95e7f
hemanialaparthi <alaparthi01@allegheny.edu>
2025-06-10 16:18:50 -0400

    feat(listener.zig): add Port Struct and complete function signatures.

---
+++ listener/listener.zig
@@ -0,0 +1,41 @@
+const std = @import("std");
+
+// PORT IMPLEMENTATION
+
+pub const Port = struct {
+    number: u16,
+
+    // list of the RESTRICTED ports
+    const required_ports = [_]u16{ 22, 80, 443, 2375, 3306, 5432, 6379, 8080, 27017 };
+
+    // min and max port range
+    const min_port = 1025;
+    const max_port = 65535;
+
+    // initialize with a specific port number
+    pub fn init(port_number: u16) Port {
+        if (isRestricted(port_number)) {
+            return error.RestrictedPort;
+        }
+
+        if (!try isAvailable(port_number)) {
+            return error.PortUnavailable;
+        }
+
+        return Port{ .number = port_number };
+    }
+
+    pub fn findAvailable() !Port {
+        // TODO: find an available port in the range
+    }
+
+    fn isRestricted(port: u16) bool {
+        // TODO: be able to check if the port is in the restricted list
+    }
+
+    fn isAvailable(port: u16) !bool {
+        // test if a port is available by trying to bind to it
+    }
+};
+
+// LISTENER IMPLEMENTATION
